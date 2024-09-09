fea56e5e8899b69577412a4479d3543d0facb14d
Meng0159 <mlee0159@student.monash.edu>
2024-09-09 21:10:23 +1000

    voter_authentication with blockchain

--- voter_authentication.py
+++ voter_authentication.py
@@ -1,10 +1,12 @@
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.asymmetric import rsa, padding 
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 import json
 import hashlib
 import os
+import random
+from blockchain_ganache import add_candidate,create_voter_account, create_voter_account, cast_vote_transact
 
 #----------------------------Voter Registration-with Storing Key Pair----------------------------------------
 
@@ -30,7 +32,7 @@ def generate_rsa_keys():
     return private_key, public_key
 
 
-# Function to register a voter
+# Function to register a voter with blockchain and store their keys and info
 def register_voter(voters):
     name = input("Enter your name: ")
     email = input("Enter your email: ")
@@ -40,19 +42,49 @@ def register_voter(voters):
         return
 
     hashed_voter_id, plain_voter_id = generate_voter_id()
-    private_key, public_key = generate_rsa_keys()
-    # Serialize the keys before storing
-    serialized_private_key = serialize_private_key(private_key)
-    serialized_public_key = serialize_public_key(public_key)
+    
+    # Blockchain: Create voter account and authorize on the blockchain index 0-9
+    ganache_index= random.randint(0, 9)
+    voter_address=create_voter_account(ganache_index)
+    print(f"Voter '{plain_voter_id}' assigned to existing address: {voter_address}")
 
+    # Generate RSA keys
+    private_key, public_key = generate_rsa_keys()
+
+    # Store voter info with blockchain address
     voters[hashed_voter_id] = {
         'name': name,
         'email': email,
         'id': plain_voter_id,
-        'private_key': serialized_private_key,
-        'public_key': serialized_public_key
+        'private_key': private_key,
+        'public_key': public_key,
+        'ganache_index': ganache_index,
+        'blockchain_address': voter_address  # Store their blockchain address
     }
-    print(f"You registered successfully with Voting_ID: {plain_voter_id}")
+    print(f"Voter registered successfully with ID: {plain_voter_id}")
+
+# def register_voter(voters):
+#     name = input("Enter your name: ")
+#     email = input("Enter your email: ")
+
+#     if is_email_registered(voters, email):
+#         print("Email already registered. One email can be used for one registration.")
+#         return
+
+#     hashed_voter_id, plain_voter_id = generate_voter_id()
+#     private_key, public_key = generate_rsa_keys()
+#     # Serialize the keys before storing
+#     serialized_private_key = serialize_private_key(private_key)
+#     serialized_public_key = serialize_public_key(public_key)
+
+#     voters[hashed_voter_id] = {
+#         'name': name,
+#         'email': email,
+#         'id': plain_voter_id,
+#         'private_key': serialized_private_key,
+#         'public_key': serialized_public_key
+#     }
+#     print(f"You registered successfully with Voting_ID: {plain_voter_id}")
 
 # Function to serialize private key
 def serialize_private_key(private_key):
@@ -122,7 +154,37 @@ def deserialize_public_key(serialized_public_key):
         serialized_public_key,
         backend=default_backend()
     )
-#--Add blinding function----------------------------------------------------------------------
+# casting a vote
+def cast_vote_and_deserialize_keyPairs(voters, vote_records, candidate_number, candidate_name):
+    voter_id = input("Enter your voter ID: ")
+    hashed_voter_id = hashlib.sha256(voter_id.encode()).hexdigest()
+    if hashed_voter_id not in voters:
+        print("Voter not registered. Please register first.")
+    else:
+        voter = voters[hashed_voter_id]
+        voter_address= voter['blockchain_address']
+        # Cast a vote from voter for a candidate and create a block in Ganache to be added in e-voting contract
+        cast_vote_transact(voter_address, candidate_number - 1)  # Candidates are zero-indexed in the contract
+        # Deserialize the private key for signing
+        private_key=voter['private_key']
+        # Deserialize the public key for encryption
+        public_key = voter['public_key']
+        # Sign and encrypt the vote with private key
+        signature = sign_vote(private_key, candidate_name)
+        # Encrypt the vote with public key
+        encrypted_vote = encrypt_vote(public_key, candidate_name)
+
+        vote_records.append({
+            'voter_id': hashed_voter_id,
+            'encrypted_vote': encrypted_vote,
+            'stored_private_key': private_key,
+            'stored': public_key,
+            'signature': signature
+        })
+        print(f"Vote cast for candidate {candidate_number} successfully!")
+
+
+#--Add blinding function for preventing double voting--------------------------------------------------------------------
 # Blinding a vote or token (done by voter)
 def blind_vote(public_key, vote):
     blinding_factor = os.urandom(16)
@@ -174,19 +236,30 @@ def verify_signature(public_key, signature, original_vote):
 def count_votes(vote_records):
     vote_count = {}
     for record in vote_records:
-        candidate = record['candidate']
+        candidate = record['vote_candidate']
         if candidate not in vote_count:
             vote_count[candidate] = 0
         vote_count[candidate] += 1
     return vote_count
 
+
+
+
 # Main function to run the voting application
 def main():
     # Define candidates
-    candidates = ["Alice", "Bob", "Charlie"]
+    candidates = {
+        1: "Alice",
+        2: "Bob",
+        3: "Charlie"
+    }
+    
 
     print("Welcome to the RSA Secure Voting System!")
-    print("Candidates:", ", ".join(candidates))
+    print("Candidates:")
+    for candidate_number, candidate_name in candidates.items():
+        add_candidate(candidate_name)
+        print(f"{candidate_number}: {candidate_name}")
 
     # Voter data storage
     voters = {}
@@ -205,40 +278,19 @@ def main():
             register_voter(voters)
 
         elif choice == '2':
-            voter_id = input("Enter your voter ID: ")
-            hashed_voter_id = hashlib.sha256(voter_id.encode()).hexdigest()
-            if hashed_voter_id not in voters:
-                print("Voter not registered. Please register first.")
-            else:
-                candidate = input("Enter your vote (choose a candidate's name): ")
-                if candidate not in candidates:
-                    print("Invalid vote. Please choose a valid candidate.")
-                else:
-                    voter = voters[hashed_voter_id]
-                    # Deserialize the private key for signing
-                    private_key = deserialize_private_key(voter['private_key'])
-                    # Deserialize the public key for encryption
-                    public_key = deserialize_public_key(voter['public_key'])
-                    # Sign and encrypt the vote with private key
-                    signature = sign_vote(private_key, candidate)
-                    # Encrypt the vote with public key
-                    encrypted_vote = encrypt_vote(public_key, candidate)
-                    
-                    vote_records.append({
-                        'voter_id': hashed_voter_id,
-                        'encrypted_vote': encrypted_vote,
-                        'signature': signature
-                    })
-                    print(f"Vote cast for {candidate} successfully!")
+            candidate_number = int(input("Enter the candidate number: "))
+            candidate_name = candidates[candidate_number]
+            print(f"Voting for candidate: {candidate_name}")
+            cast_vote_and_deserialize_keyPairs(voters, vote_records, candidate_number,candidate_name)
 
         elif choice == '3':
             print("\nDecrypting and counting votes...")
             for record in vote_records:
                 voter_id = record['voter_id']
-                private_key = deserialize_private_key(voter['private_key'])
+                private_key = record['stored_private_key']
                 encrypted_vote = record['encrypted_vote']
                 decrypted_vote = decrypt_vote(private_key, encrypted_vote)
-                record['candidate'] = decrypted_vote
+                record['vote_candidate'] = decrypted_vote
 
             vote_count = count_votes(vote_records)
             print("\n--- Total Votes ---")
